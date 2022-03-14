17233d1e04b4a59c0c168f0c20da9e66b7d4956b
praveen5959 <praveenbhat98@gmail.com>
2022-03-14 16:36:43 +0530

    Added project structure, Header and Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="layout">
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<p>Home</p>} />
+            <Route path="/complete" element={<p>Completed</p>} />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="nav">
+      <div className="logo">
+        <h1> Sticky Notes</h1>
+      </div>
+      <div className="links">
+        <ul>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "link")}
+            to="/"
+          >
+            <li>
+              <h3>All Notes</h3>
+            </li>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "link")}
+            to="/complete"
+          >
+            <li>
+              <h3>Completed</h3>
+            </li>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
